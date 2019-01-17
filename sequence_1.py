8679f5c892b4e38a1a1acff1838de80bdd4a86de
ZhangZhao <30930207+hummingzz@users.noreply.github.com>
2019-01-17 12:59:08 +0800

    1
    1

---
+++ sequence_1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import os
+import shutil
+
+df = pd.read_csv('train_processed.csv')
+df_train = df.sample(frac=0.9,random_state=0)
+print(df_train.index)
+df_test = df.drop(df_train.index)
+print(df_test.shape)
+# np.arange(len(df)*0.9)
+
+for i in df_test['Image']:
+    shutil.move('../home/lchn_guo/projects/WhalesServer/generated_train/'+ str(i) ,'../home/lchn_guo/projects/WhalesServer/generated_train/test/' + str(i))
+
+#shutil.move('white_1.jpg', './New_data/move_1.jpg')
